e107338b319e76648bdec17d1b2e53619018f15b
Joe Alt <joejoyalt@gmail.com>
2023-07-02 15:58:58 +0200

    incorporation

--- src/views/UI/borderComposition/BorderComposition.jsx
+++ src/views/UI/borderComposition/BorderComposition.jsx
@@ -3,12 +3,9 @@ import './borderComposition.scss'
 
 const BorderComposition = () => {
   return (
-    <div className="border-composition">
-      <svg height="64" width="324" xmlns="http://www.w3.org/2000/svg">
-        <rect className="shape" x="2" y="2" height="60" width="320" rx="4px" ry="4px" stroke-linejoin="round" />
-      </svg>
-      <input type="text" placeholder="Nice to see you" className="input" />
-    </div>
+    <svg height="64" width="454" xmlns="http://www.w3.org/2000/svg">
+      <rect className="shape" x="2" y="2" height="60" width="450" rx="4px" ry="4px" stroke-linejoin="round" />
+    </svg>
   )
 }
 
