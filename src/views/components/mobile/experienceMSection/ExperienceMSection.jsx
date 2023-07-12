224942c5e169c4af604e25efb8e126c0d22da704
Joe Alt <joejoyalt@gmail.com>
2023-07-12 16:46:04 +0200

    Feat: work experinece swiper

--- src/views/components/mobile/experienceMSection/ExperienceMSection.jsx
+++ src/views/components/mobile/experienceMSection/ExperienceMSection.jsx
@@ -1,45 +1,89 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import { useTranslation } from 'react-i18next'
 import { WorkExperience } from '@/assets/JSON/Experience'
-import MobileTabMenu from '@/assets/SVG/MobileTabMenu'
 import './experienceMSection.scss'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import { Autoplay, Pagination, Navigation, FreeMode, Thumbs } from 'swiper';
 
 const ExperienceMSection = () => {
   const { t } = useTranslation();
   const jsonObject = WorkExperience();
-  const [active, setActive] = useState(jsonObject[0].id);
-  const [handleClick, setHandleClick] = useState(false)
+  const [thumbsSwiper, setThumbsSwiper] = useState(null);
+  const progressCircle = useRef(null);
+  const progressContent = useRef(null);
+  const onAutoplayTimeLeft = (s, time, progress) => {
+    progressCircle.current.style.setProperty('--progress', 1 - progress);
+    progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
+  };
 
   return (
     <>
       <div className="experience-section-mobile-header">
         <span>02. </span><h2>{t("experienceSection_header")}</h2>
       </div>
-      <div className="experience-section-mobile-nav">
-        <button onClick={() => setHandleClick(!handleClick)} className="experience-section-mobile-nav__burger">
-          <MobileTabMenu status={handleClick} />
-        </button>
-      </div>
       <div className="experience-section-mobile-body">
-        {jsonObject?.map((experience) => {
-          const { id, business, position, duration, description, tools } = experience;
-          if (active === id) {
+        <Swiper
+          centeredSlides={true}
+          loop={true}
+          autoplay={{
+            delay: 8500,
+            disableOnInteraction: true
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          thumbs={{ swiper: thumbsSwiper }}
+          modules={[Autoplay, Pagination, Navigation, Thumbs]}
+          onAutoplayTimeLeft={onAutoplayTimeLeft}
+          className="mySwiper"
+        >
+          {jsonObject?.map((experience) => {
+            const { id, business, position, duration, description, tools } = experience;
             return (
-              <section key={id} className="experience-section-mobile-body__wrap">
-                <h4 className="experience-section-mobile-body__wrap--title">{position} <b>@ {business}</b></h4>
-                <p className="experience-section-mobile-body__wrap--date">{duration}</p>
-                <p>{description}</p>
-                <div className="experience-section-mobile-body__wrap--tools">
-                  {tools?.map((tool, index) => {
-                    return (
-                      <span key={index} className="experience-section-mobile-body__wrap--tools__tool">{tool}</span>
-                    )
-                  })}
-                </div>
-              </section>
+              <SwiperSlide>
+                <section key={id} className="experience-section-mobile-body__wrap">
+                  <h4 className="experience-section-mobile-body__wrap--title">{position} <b>@ {business}</b></h4>
+                  <p className="experience-section-mobile-body__wrap--date">{duration}</p>
+                  <p>{description}</p>
+                  <div className="experience-section-mobile-body__wrap--tools">
+                    {tools?.map((tool, index) => {
+                      return (
+                        <span key={index} className="experience-section-mobile-body__wrap--tools__tool">{tool}</span>
+                      )
+                    })}
+                  </div>
+                </section>
+              </SwiperSlide>
             )
-          }
-        })}
+          })}
+          <div className="autoplay-progress" slot="container-end">
+            <svg viewBox="0 0 48 48" ref={progressCircle}>
+              <circle cx="24" cy="24" r="20"></circle>
+            </svg>
+            <span ref={progressContent}></span>
+          </div>
+        </Swiper>
+        <Swiper
+          onSwiper={setThumbsSwiper}
+          slidesPerView={5}
+          loop={true}
+          spaceBetween={10}
+          watchSlidesProgress={true}
+          modules={[Thumbs]}
+          className="mySwiper"
+        >
+          {jsonObject?.map((experience) => {
+            const { short } = experience;
+            return (
+              <SwiperSlide>
+                <p>{short}</p>
+              </SwiperSlide>
+            )
+          })}
+        </Swiper>
       </div>
     </>
   )
