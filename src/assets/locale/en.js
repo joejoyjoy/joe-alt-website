507f2d24aac160cc0944b9d993e5d6188e37b0f4
Joe Alt <joejoyalt@gmail.com>
2023-07-23 17:58:43 +0200

    Grammar check

--- src/assets/locale/en.js
+++ src/assets/locale/en.js
@@ -20,20 +20,20 @@ const englishTranslation = {
   // HERO SECTION
   heroSection_welcome: "Hi, my name is",
   heroSection_position: "Full Stack Web Developer.",
-  heroSection_description: "I'm a software developer specialized in web development building exceptional frontend and backend web experiences. Currently, I'm focusing on learning more JavaScript libraries and frameworks to improve my next projects and also actively searching for a job opportunity.",
+  heroSection_description: `I'm a software developer specializing in web development, building exceptional frontend and backend web experiences. Currently, I'm focusing on 
+  learning more JavaScript libraries and frameworks to improve my next projects and am also actively searching for a job opportunity.`,
   heroSection_btns_action: "Get In Touch",
   heroSection_btns_download: "My Resume",
 
   // ABOUT SECTION
   aboutSection_header: "About Me",
-  aboutSection_body_span1: `Hey! I'm Joe, always deeply passionate about web-driven technologies to create a positive impact on customers' daily lives. I have been building <bold>real-case websites</bold> since 2019. 
-  I'm thankful to have been working as a <bold>freelancer</bold> for several years creating websites of all kinds, each with different functionalities involved. 
-  For example, product filtering and sorting, online payments, and connecting with logistics trough API, among others.`,
-  aboutSection_body_span2: `After creating several successful websites for different businesses, 
-  I started a <aTag>Master's program</aTag> in <bold>Full Stack Web Development</bold> in 2022 
-  to improve and learn more advanced technologies along with implementing <bold>Agile methodologies</bold> like <bold>Scrum</bold>.`,
-  aboutSection_body_span3: `Fast-forward to today, my main focus is building <bold>accessible websites</bold> for all to further improve my knowledge, experience and my passion. 
-  I'm currently searching for a Job position as a <bold>Full Stack Web Developer</bold> or <bold>Front-end Web Developer</bold> where I can continue to grow.`,
+  aboutSection_body_span1: `Hey! I'm Joe, always deeply passionate about web-driven technologies that create a positive impact on customers' daily lives. I have been building 
+  <bold>real-case websites</bold> since 2019. I'm thankful to have been working as a <bold>freelancer</bold> for several years, creating websites of all kinds, each with 
+  different functionalities involved. For example, product filtering and sorting, online payments, and connecting with logistics through an API, among others.`,
+  aboutSection_body_span2: `After creating several successful websites for different businesses, I started a Master's program in <aTag>Master's program</aTag> in 
+  <bold>Full Stack Web Development</bold> in 2022 to improve and learn more advanced technologies, along with implementing <bold>Agile methodologies</bold> like <bold>Scrum</bold>.`,
+  aboutSection_body_span3: `Fast-forward to today, and my main focus is building <bold>accessible websites</bold> for all to further improve my knowledge, experience, and passion. 
+  I'm currently searching for a Job position as a <bold>Full Stack Web Developer</bold> or <bold>Front-end Web Developer</bold>  where I can continue to grow.`,
   aboutSection_body_span4: "Here are a few technologies I've been working with recently:",
   aboutSection_image_alt: "Portrait picture of Joe Alt website developer",
   aboutSection_image_location: "Malaga, Spain",
@@ -79,30 +79,30 @@ const englishTranslation = {
   workExperienceJson_position_developer: "Full Stack Web Developer",
   workExperienceJson_position_builder: "Freelance Web Builder",
   workExperienceJson_position_backend: "Freelance Backend Developer",
-  workExperienceJson_description_01: `I joined this Master focused in teaching Full Stack Web Development by building with a team 
-  real-case projects from scratch, 1200 hours, full-time and remotely. I have done +20 real-world projects with 
-  a deep focus in MERN and LAMP stacks. Also using Agile development techniques like Scrum. I learned how to 
-  applying clean code practices and scalability patterns. Learned how to work in a team, and was Team Leader twice 
-  using all my knowledge to guide my team. After graduation I was able to create an awesome website without any website 
-  builder, pure code. Which makes me more able to create more personalized websites from now on`,
-  workExperienceJson_description_02: `I was the UI/UX designer, website builder and maintainer of the site. I have learned 
-  how to implement an appointment system with a calendar to book an appointment with the masseuse. 
-  I made it mobile friendly, and made it SEO optimized for best ranking results of Google and Bing.`,
-  workExperienceJson_description_03: `I was the backend web developer at Priccesa eCommerce site. PHP was necessary to 
-  communicate with their logistics provider. I learned how to implement PHP to ensure a stable connection 
-  between the two businesses. I was also responsible for the correct uploading of the products 
+  workExperienceJson_description_01: `I joined this Master's program to learn in depth about Full Stack Web Development by building with a team.
+  real-case projects from scratch, 1200 hours full-time and remotely. I have done 20 real-world projects with
+  a deep focus on MERN and LAMP stacks. Also using Agile development techniques like Scrum. I learned how to
+  applying clean code practices and scalability patterns. I learned how to work in a team and was Team Leader twice.
+  using all my knowledge to guide my team. After graduation, I was able to create an awesome website without any
+  builder, pure code Which makes me more able to create more personalized websites from now on.`,
+  workExperienceJson_description_02: `I was the UI/UX designer, website builder, and maintainer of the site. I have learned
+  how to implement an appointment system with a calendar to book an appointment with the masseuse.
+  I made it mobile-friendly and SEO-optimized for the best ranking results on Google and Bing.`,
+  workExperienceJson_description_03: `I was the backend web developer at the Priccesa eCommerce site. PHP was necessary to
+  communicate with their logistics provider. I learned how to implement PHP to ensure a stable connection.
+  between the two businesses. I was also responsible for the correct uploading of the products.
   into the database and secure payment methods with a cancellation policy.`,
-  workExperienceJson_description_04: `I learned  how to develop a website for my client with HTML5, CSS3 and JavaScript.
-  I was responsible for developing, maintaining and making it SEO optimized. I learned how to 
-  use new technologies, create a fast loading site with cross-browser compatibility and mobile 
-  responsiveness. It was about a restaurant so I used Google Maps, Apple Maps, Google Analytics, 
-  Google Ads, Semrush, Google Search Console and Bing Webmaster to ensure user optimized experience 
+  workExperienceJson_description_04: `I learned how to develop a website for my client with HTML5, CSS3, and JavaScript.
+  I was responsible for developing, maintaining, and making it SEO-optimized. I learned how to 
+  Use new technologies to create a fast-loading site with cross-browser compatibility and mobile 
+  responsiveness. It was about a restaurant, so I used Google Maps, Apple Maps, Google Analytics,
+  Google Ads, Semrush, Google Search Console, and Bing Webmaster to ensure a user-optimized experience.
   and high SEO ranking.`,
-  workExperienceJson_description_05: `I learned using a cloud-based website builder and started my journey from there. 
-  I was the UI/UX designer, website builder and maintainer. I learned to take technical decisions on my own, 
-  building a website that's easy to use and understand. Helping the wireframes and sketches I made before.
-  I implemented external secure payment methods and used JavaScript to create advanced sorting and filtering 
-  features from scratch. In the end this was the project that made me realize the passion I have for 
+  workExperienceJson_description_05: `I learned using a cloud-based website builder and started my journey from there.
+  I was the UI/UX designer, website builder, and maintainer. I learned to take technical decisions on my own.
+  building a website that's easy to use and understand. helping with the wireframes and sketches I made before.
+  I implemented external secure payment methods and used JavaScript to create advanced sorting and filtering.
+  features from scratch. In the end, this was the project that made me realize the passion I have for
   web development, and it surely didn't end there.`,
   workExperienceJson_website_02_title: `Massage and Spa at Home | Massages & Wellness at Home`,
   workExperienceJson_website_05_title: `Health Retreats | Andalusian Health Retreats | Malaga`,
@@ -119,20 +119,20 @@ const englishTranslation = {
   workCases_name_03: "M&G - GIPHY Clone",
   workCases_name_04: "Hangman Game - Time For Fun",
   workCases_name_05: "Hopper VPN - OOP PHP CRUD",
-  workCases_description_01: `As a team of 5 developers we pulled of a Spotify clone with all the key functions. I was 
-  responsible for the frontend design and the logic for most of the functions together with consuming the backend server 
-  as intended. The application is mobile friendly and is installable trough the browser.`,
-  workCases_description_02: `This was a coding challenge of 6 days max. It's about consuming the v4 API of GitHub with GraphQL. 
-  You can login with your GitHub account and then you will see your followers and some statistics about yourself and as well 
-  all the repositories public available on your account. You can search and sort by A-Z, date and last modified.`,
-  workCases_description_03: `This was a coding challenge of 3 days max. It's about consuming the GIPHY API and be able to 
-  upload your own GIFs into de database. You can upload GIFs trough local disk or external URL. I have added a 3D spline 
-  asset giving a immersive experience`,
-  workCases_description_04: `This was one of my first projects with 3 other developers. We created a game with vanilla 
-  JavaScript. It's hangman, check it out because it has some funny graphics.`,
-  workCases_description_05: `As a team of 4 developers we pulled of a Object Oriented Program with PHP. It's about a 
-  program were you can connect to VPNs from the client side or create, edit or delete them as a administrator. I was the team 
-  leader and it was all build within a week. Note that it doesn't connect you to any VPNs, only visual graphics used.`,
+  workCases_description_01: `As a team of five developers, we pulled off a Spotify clone with all the key functions. I was
+  responsible for the frontend design and the logic for most of the functions, together with consuming the backend server.
+  as intended. The application is mobile-friendly and is installable through the browser.`,
+  workCases_description_02: `This was a coding challenge of 6 days max. It's about consuming the v4 API of GitHub with GraphQL.
+  You can login with your GitHub account, and then you will see your followers and some statistics about yourself as well.
+  all the public repositories available on your account. You can search and sort by A-Z, date, and last modified.`,
+  workCases_description_03: `This was a coding challenge of 3 days max. It's about consuming the GIPHY API and being able to
+  Upload your own GIFs to the database. You can upload GIFs through your local disk or an external URL. I have added a 3D spline.
+  asset, giving an immersive experience.`,
+  workCases_description_04: `This was one of my first projects with three other developers. We created a game with vanilla
+  JavaScript. It's Hangman; check it out because it has some funny graphics.`,
+  workCases_description_05: `As a team of four developers, we built an object-oriented program with PHP. It's about a
+  program where you can connect to VPNs from the client side or create, edit, or delete them as an administrator. I was on the team.
+  leader, and it was all built within a week. Note that it doesn't connect you to any VPNs; only visual graphics are used.`,
 
 
 
